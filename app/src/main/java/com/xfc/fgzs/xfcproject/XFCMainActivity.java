91f9f370c14e5aef32a8640bb2f8bdd078367b55
cfjgithub <imissyouyw@126.com>
2017-11-05 21:56:27 +0800

    dd

--- app/src/main/java/com/xfc/fgzs/xfcproject/XFCMainActivity.java
+++ app/src/main/java/com/xfc/fgzs/xfcproject/XFCMainActivity.java
@@ -3,11 +3,15 @@ package com.xfc.fgzs.xfcproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+* Bundle类是一个key-value对
+* 两个activity之间的通讯可以通过bundle类来实现
+* */
 public class XFCMainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xfcmain);
     }
