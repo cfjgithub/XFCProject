b97d81165ed5289befb30c2e0bb7ef1a878341d2
cfjgithub <imissyouyw@126.com>
2017-11-05 23:04:38 +0800

    commit

--- app/src/main/java/com/xfc/fgzs/xfcproject/XFCMainActivity.java
+++ app/src/main/java/com/xfc/fgzs/xfcproject/XFCMainActivity.java
@@ -11,7 +11,6 @@ public class XFCMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xfcmain);
     }
